d914ccf5407ba96292ef73645c20e8887f77e424
Phinehas <walburphinehas78@gmail.com>
2025-06-04 15:12:49 +0300

    Implemented views for my store app

--- store/views.py
+++ store/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import SafeProduct, StoreLocation
 
 # Create your views here.
+def product_list(request):
+    products = SafeProduct.objects.all()
+    return render(request, 'store/product_list.html', {'products': products})
+
+def product_detail(request, slug):
+    product = get_object_or_404(SafeProduct, slug=slug)
+    return render(request, 'store/product_detail.html', {'products': products})
+
+def store_locations(request):
+    locations = StoreLocation.objects.all()
+    return render(request, 'store/locations.html', {'locations': locations})
