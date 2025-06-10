5a76f543707f9eb56cc3d35eb0eebd5d09844f52
Phinehas <walburphinehas78@gmail.com>
2025-06-10 17:15:17 +0300

    Configure store admin

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import SafeProduct, StoreLocation, ProductSpecification
 
-# Register your models here.
+
+class SafeProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price']
+    class Media:
+        css = {
+            'all': ('/static/css/ckeditor.css',)
+            
+        }
+
+    def description_preview(self, obj):
+        return obj.description
+    description_preview.allow_tags = True
+
+admin.site.register(SafeProduct, SafeProductAdmin)
+admin.site.register(StoreLocation)
+admin.site.register(ProductSpecification)
