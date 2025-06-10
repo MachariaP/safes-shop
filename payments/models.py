ff65ca2d85c7fa2e28cf498d7e98b6abb23cbbf4
Phinehas <walburphinehas78@gmail.com>
2025-06-10 17:15:16 +0300

    Implement payment models

--- payments/models.py
+++ payments/models.py
@@ -17,8 +17,7 @@ class MpesaTransaction(models.Model):
     merchant_request_id = models.CharField(max_length=100)
     mpesa_receipt_number = models.CharField(max_length=100, blank=True)
     transaction_date = models.DateTimeField(null=True, blank=True)
-    status = models.CharField(max_length=20, choice=TRANSACTION_STATUS,
-                              default='pending')
+    status = models.CharField(max_length=20, choices=TRANSACTION_STATUS, default='pending')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
