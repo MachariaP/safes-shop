66d1d985c9e760c498804529509474c4f76f2db3
Phinehas <walburphinehas78@gmail.com>
2025-06-11 14:06:25 +0300

    Added debug_session URL and confirmed update_cart pattern

--- store/urls.py
+++ store/urls.py
@@ -1,19 +1,19 @@
-from django.urls import path, re_path
-from django.shortcuts import redirect
+from django.urls import path
 from . import views
 
 app_name = 'store'
 
 urlpatterns = [
     path('', views.product_list, name='product_list'),
-    path('locations/', views.store_locations, name='store_locations'),
-    path('product-<str:slug>/', views.product_detail, name='product_detail'),
     path('<str:category>/', views.product_list, name='category_product_list'),
-    re_path(r'^store/(?P<slug>[-\w]+)/$', lambda request, slug: redirect('store:product_detail', slug=slug, permanent=True)),
+    path('product-<str:slug>/', views.product_detail, name='product_detail'),
     path('add-to-cart/<str:slug>/', views.add_to_cart, name='add_to_cart'),
     path('add-to-wishlist/<str:slug>/', views.add_to_wishlist, name='add_to_wishlist'),
     path('cart/', views.cart_view, name='cart'),
     path('update-cart/', views.update_cart, name='update_cart'),
     path('checkout/', views.CheckoutView.as_view(), name='checkout'),
     path('order-confirmation/<int:order_id>/', views.order_confirmation, name='order_confirmation'),
+    path('locations/', views.store_locations, name='store_locations'),
+    path('newsletter-signup/', views.newsletter_signup, name='newsletter_signup'),
+    path('debug-session/', views.debug_session, name='debug_session'),
 ]
