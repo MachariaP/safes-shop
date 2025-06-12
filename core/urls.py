f1e188bcf2be354a36e6288f39420f8f8220e9c4
Phinehas <walburphinehas78@gmail.com>
2025-06-12 15:17:40 +0300

    Added URL configuration for contact page

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import AboutUsView
+from .views import AboutUsView, ContactView
 
 app_name = 'core'
 
 urlpatterns = [
     path('about-us/', AboutUsView.as_view(), name='about_us'),
+    path('contact/', ContactView.as_view(), name='contact'),
 ]
