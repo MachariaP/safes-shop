314e378ff533f77d357e617f284df212cc9192d1
Phinehas <walburphinehas78@gmail.com>
2025-06-18 16:50:42 +0300

    Update store/admin.py: Adjusted admin configuration to support new icon dropdown in ProductSpecification inline, ensuring proper display of icon choices in the admin interface.

--- store/admin.py
+++ store/admin.py
@@ -1,19 +1,61 @@
+# store/admin.py
+
+"""Admin configuration for the store app models."""
+
 from django.contrib import admin
-from .models import SafeProduct, StoreLocation, ProductSpecification
+from .models import SafeProduct, ProductSpecification, StoreLocation, WishlistItem, Order, OrderItem
 
 
+class ProductSpecificationInline(admin.TabularInline):
+    """Inline admin for ProductSpecification to manage specs within SafeProduct."""
+    model = ProductSpecification
+    extra = 4  # Provide 4 fields for Dimensions, Weight, Capacity, Fire Class
+    fields = ('name', 'value', 'icon')
+    ordering = ('name',)
+
+
+@admin.register(SafeProduct)
 class SafeProductAdmin(admin.ModelAdmin):
-    list_display = ['name', 'price']
-    class Media:
-        css = {
-            'all': ('/static/css/ckeditor.css',)
-            
-        }
+    """Admin configuration for SafeProduct model."""
+    list_display = ('name', 'category', 'price', 'is_in_stock', 'created_at')
+    list_filter = ('category', 'is_in_stock')
+    search_fields = ('name', 'description')
+    prepopulated_fields = {'slug': ('name',)}
+    inlines = [ProductSpecificationInline]
 
-    def description_preview(self, obj):
-        return obj.description
-    description_preview.allow_tags = True
 
-admin.site.register(SafeProduct, SafeProductAdmin)
-admin.site.register(StoreLocation)
-admin.site.register(ProductSpecification)
+@admin.register(ProductSpecification)
+class ProductSpecificationAdmin(admin.ModelAdmin):
+    """Admin configuration for ProductSpecification model."""
+    list_display = ('product', 'name', 'value', 'icon')
+    list_filter = ('name',)
+    search_fields = ('product__name', 'name', 'value')
+
+
+@admin.register(StoreLocation)
+class StoreLocationAdmin(admin.ModelAdmin):
+    """Admin configuration for StoreLocation model."""
+    list_display = ('name', 'address', 'is_primary')
+    search_fields = ('name', 'address')
+
+
+@admin.register(WishlistItem)
+class WishlistItemAdmin(admin.ModelAdmin):
+    """Admin configuration for WishlistItem model."""
+    list_display = ('product', 'added_at')
+    search_fields = ('product__name',)
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    """Admin configuration for Order model."""
+    list_display = ('order_number', 'user', 'total_price', 'payment_method', 'is_paid', 'created_at')
+    list_filter = ('payment_method', 'is_paid')
+    search_fields = ('order_number', 'contact_email', 'contact_phone')
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    """Admin configuration for OrderItem model."""
+    list_display = ('order', 'product', 'quantity', 'price')
+    search_fields = ('order__order_number', 'product__name')
